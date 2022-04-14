31e979b7718fdbc3340139914bd110a633e28fe1
Débora Valentim <54693399+deboranav@users.noreply.github.com>
2022-04-14 20:28:08 -0300

    Update function.cpp

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -9,7 +9,8 @@ std::vector<unsigned int> fib_below_n( unsigned int n ){
     
       vetor.push_back(1);
       vetor.push_back(1);
-
+      
+      cin >> n; 
 
       while ((vetor[i-1] + vetor[i-2]) <= n){
           vetor.push_back(vetor[i-1] + vetor[i-2]) ;
