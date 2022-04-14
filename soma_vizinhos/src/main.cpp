ae39e302fc6081671b2a6d8fc8acf7dfd95b972a
Débora Valentim <54693399+deboranav@users.noreply.github.com>
2022-04-14 20:59:01 -0300

    Update main.cpp

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -1,8 +1,3 @@
-/*!
- * @brief This code implements the "Soma Vizinhos" programming problem
- * @author selan
- * @data June, 6th 2021
- */
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -10,7 +5,34 @@ using std::endl;
 
 int main( void )
 {
-    // TODO: Adicione seu código aqui. 
+    int m, n, i;
+    int soma = 0;
+
+    cin >> m;
+
+    cin >> n;
+
+    if (n > 0){
+
+      for (i = 0; i < n; i++){
+        soma = soma + (m + i); 
+      }
+
+    }
+
+    if (n < 0){
+
+      n = n * -1;
+
+      for (i = 0; i < n; i++){
+
+        soma = soma + (m - i); 
+      }
+
+    }
+    
+    cout << soma; 
+    
 
     return 0;
 }
