ef72c51cdb88c93eaefee1f3af5ebb6e7cee0458
Débora Valentim <54693399+deboranav@users.noreply.github.com>
2022-04-14 20:58:20 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,7 +6,19 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int valor,  i;
+    int contador = 0;
 
+    for (i = 0; i < SIZE ; i++){
+      cin >> valor;
+
+      if (valor < 0){
+        contador += 1;
+      }
+
+    }
+    
+    cout << contador;
+  
     return 0;
 }
